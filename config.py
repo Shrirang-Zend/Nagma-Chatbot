628e7afcfac32cd6b302b8ee5c41ba5ae6aeb21a
Shrirang-Zend <128277571+Shrirang-Zend@users.noreply.github.com>
2024-10-02 19:25:15 +0530

    config file

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-DATA_PATH = '/Users/shrirangzend/Desktop/Mundane College Subjects/MPR/Sem5 MPR/Nagma-Chatbot/data.csv'  # Update this path to the location of your dataset
+DATA_PATH = 'data.csv'  # Update this path to the location of your dataset
